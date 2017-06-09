48c0510fb59272aa8024ffd5528459fe8ffe19e5
Joseph Harry <findarato@gmail.com>
2017-06-09 10:53:33 -0500

    :bug:fix(zsh and linking): Linking once again actually does something
    Signed-off-by: Joseph Harry <findarato@gmail.com>

--- linkup.sh
+++ linkup.sh
@@ -5,3 +5,15 @@
 # It will remove current files and then create the
 # symlinks
 #
+
+
+
+
+for FILE in .*; do
+    if [ "$FILE" != .travis.yml ] && [ "$FILE" != .git ] && [ "$FILE" != . ] && [ "$FILE" != .. ]; then
+        # echo "$FILE"
+        echo "Linking Files: $PWD/$FILE ==> $HOME/$FILE"
+        rm -f "$HOME/$FILE"
+        ln -s "$PWD/$FILE" "$HOME/$FILE"
+    fi
+done
