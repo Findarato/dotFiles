7515739bf09627d10fce1a8878d7722f6c1a251b
Joseph Harry <findarato@gmail.com>
2017-08-14 10:25:22 -0500

    chore(cleanup): Removed a LOT of old scripts that were not really needed Signed-off-by: Joseph Harry <findarato@gmail.com>

--- bin/mountWPLSTATS.sh
+++
@@ -1,2 +0,0 @@
-sshfs -o allow_other,IdentityFile=~/.ssh/orchestrator joe@192.168.1.12:/var/www/html /home/joe/mnt/wpl-stats
-
--- bin/mountWplStatsSSHfs.sh
+++
@@ -1,2 +0,0 @@
-sudo sshfs -o allow_other,IdentityFile=/home/joe/.ssh/remote joe@192.168.1.12:/var/www/html/ /mnt/wpl-stats/
-#sshfs -o IdentityFile=/home/joe/.ssh/remote,auto_cache,reconnect joe@wpl-stats:/var/www/html/ /mnt/wpl-stats
