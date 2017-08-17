48fd75a739314e8ddfc7ac064eba0189469b15e8
Joseph Harry <findarato@gmail.com>
2017-08-17 08:06:21 -0500

    :bug:fix(beforeBackup): Fixed before backup to properly close browsers Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 7dc9326d1d00e036f1d9819b3f5f46555c9e2727

--- bin/beforeBackup.sh
+++ bin/beforeBackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-killall -9 firefox
-killall -9 chrome
-killall -9 chromium-browse
-killall -9 vivaldi-bin
-NOW=$(date +%A.%H)
+pkill --signal 9 firefox
+pkill --signal 9 chrome
+pkill --signal 9 chromium
+pkill --signal 9 vivaldi
+#NOW=$(date +%A.%H)
 #updateRepos.sh
