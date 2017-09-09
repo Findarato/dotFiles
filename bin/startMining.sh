10f09d9f8af5db8b44dd9b32e52c65d5ddb23512
Joseph Harry <findarato@gmail.com>
2017-09-09 18:01:02 -0500

    :bug:fix(VS-Code): Reverted commented out data Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 3393c8886709a5e5b375c3d4c8486c8ed4a14270

--- bin/startMining.sh
+++ bin/startMining.sh
@@ -1,5 +1,16 @@
 docker rm cuda_miner
+docker rm cuda_miner_monero
 
-nvidia-docker run --name cuda_miner -it findarato/docker-ethminer \
+docker run --name docker_miner_monero -itd \
+-e username=48tXt7CbtuwaCQnjY5LRx3VCAxbce96rd7TdAhQzWR8ZTY3vEvUuxXpUjSh1qSBhjMAb52hMumPBpHyDqrtc4tjdCqwmy9U \
+-e email=example@example.com servethehome/monero_dwarfpool:nvidia
+
+
+nvidia-docker run \
+--name cuda_miner \
+-it servethehome/ethereum:cuda \
 -S eth-us-east1.nanopool.org:9999 \
--O 0xf5ceb3d2e78ee027d93aa18f9129c7988d54a159.it01/Findarato@gmail.com
+-O 0xf5ceb3d2e78ee027d93aa18f9129c7988d54a159.\
+dev-compute01/\
+Findarato@gmail.com
+
