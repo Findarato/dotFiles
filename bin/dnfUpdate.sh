8ed31ff136187a84728994d195a3beab302c9950
Joseph Harry <findarato@gmail.com>
2018-01-07 08:23:42 -0600

    Updated some basic things Signed-off-by: Joseph Harry <findarato@gmail.com>

--- bin/dnfUpdate.sh
+++ bin/dnfUpdate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+dnf upgrade --refresh;
 dnf update -y;
 dnf distro-sync -y;
--- bin/startSync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-screen -d -m bash -c "/home/joe/Downloads/BitTorrent-Sync_x64/btsync --webui.listen 0.0.0.0:8888"
-
-
