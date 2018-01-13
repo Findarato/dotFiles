88f1e29fa7327e71454cd107c284b71fc0761e3e
Joseph Harry <findarato@gmail.com>
2018-01-13 17:44:03 -0600

    :arrow_up:feat(vfio): Things to make vfio easier to use Signed-off-by: Joseph Harry <findarato@gmail.com>

--- bin/connectToWork.sh
+++ bin/connectToWork.sh
@@ -1,1 +1,2 @@
-ssh joe@ssh.wpl.lib.in.us -L 3390:wpl-vmhost01.library.wpl.lib.in.us:3389
+#ssh joe@ssh.wpl.lib.in.us -L 3390:wpl-vmhost01.library.wpl.lib.in.us:3389
+ssh joe@ssh.wpl.lib.in.us -L 3390:wpl-dc1.library.wpl.lib.in.us:3389
