795c3b8032823dd992e6e2592c5b2ce4cfe12db0
Joseph Harry <findarato@gmail.com>
2018-01-27 21:04:26 -0600

    :bug: fix(things): Updated stuff Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 92ab4f8dbe35070ddb892b293ad2d49a929c3a18

--- bin/connectToWork.sh
+++ bin/connectToWork.sh
@@ -1,2 +1,3 @@
-#ssh joe@ssh.wpl.lib.in.us -L 3390:wpl-vmhost01.library.wpl.lib.in.us:3389
-ssh joe@ssh.wpl.lib.in.us -L 3390:wpl-dc1.library.wpl.lib.in.us:3389
+#ssh joe@ssh.wpl.lib.in.us -L 3390:wpl-vmhost02.library.wpl.lib.in.us:3389
+ssh it@gitlab.bitsandbytes.academy -L 3390:wpl-vmhost02.library.wpl.lib.in.us:3389
+#ssh joe@ssh.wpl.lib.in.us -L 3390:wpl-dc1.library.wpl.lib.in.us:3389
