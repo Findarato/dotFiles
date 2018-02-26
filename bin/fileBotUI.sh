a203370f060019cbe7c2e3d75e46225863dc1fff
Joseph Harry <findarato@gmail.com>
2018-02-26 08:10:08 -0600

    :fire: Updated things
    - Lots of stuff has been updated
    
    Former-commit-id: 6447f1e416796e82ecd18e5e2638801a1a7d5179

--- bin/fileBotUI.sh
+++
@@ -1,9 +0,0 @@
-docker run --name=FileBotUI \
--e WIDTH=1280 \
--e HEIGHT=720 \
--p 3389:3389 \
--p 8080:8080 \
--v /mnt/tarvalon/storage/Movies:/media:rw \
--v /docker/containers/FileBotUi/config:/config:rw \
-coppit/filebot
-
