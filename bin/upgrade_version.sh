8170315a0773dc141110155db8d3a36cf44e25db
Joseph Harry <findarato@gmail.com>
2018-07-03 14:29:03 -0500

    :bug: fix(zsh): Finally merged the two zsh configs so they work Signed-off-by: Joseph Harry <findarato@gmail.com>

--- bin/upgrade_version.sh
+++ bin/upgrade_version.sh
@@ -1,7 +1,7 @@
-sudo dnf upgrade --refresh
+sudo dnf upgrade -y --refresh
 
-sudo dnf install dnf-plugin-system-upgrade
+sudo dnf install -y dnf-plugin-system-upgrade
 
-sudo dnf system-upgrade download --releasever=27
+sudo dnf system-upgrade download -y --releasever=28 --allowerasing
 
-sudo dnf system-upgrade reboot
+#sudo dnf system-upgrade reboot
