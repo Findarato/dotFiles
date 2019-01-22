81a443348b5e6aab18e71431c56de3b96bb0e294
Joseph Harry <findarato@gmail.com>
2019-01-22 17:50:29 -0600

    Adding in desktop changes
    Signed-off-by: Joseph Harry <findarato@gmail.com>
    
    Former-commit-id: 64937f93a86552730278e3541ce397a9aa715465

--- bin/removeTracker.sh
+++ bin/removeTracker.sh
@@ -1,5 +1,3 @@
 tracker daemon -t
-cd ~/.config/autostart
-cp -v /etc/xdg/autostart/tracker-* ./
-for FILE in `ls tracker-*`; do echo Hidden=true >> $FILE; done
-rm -rf ~/.cache/tracker ~/.local/share/tracker
+cp /etc/xdg/autostart/tracker* ~/.config/autostart/
+sed -i 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/' ~/.config/autostart/tracker*
