a872e64ebc3b623e3b549041d90cba221007e404
Joseph Harry <findarato@gmail.com>
2019-02-20 16:27:48 -0600

    Fixed some stuf Signed-off-by: Joseph Harry <findarato@gmail.com>

--- bin/Backup.sh
+++ bin/Backup.sh
@@ -10,14 +10,14 @@ export RESTIC_PASSWORD=$(pass computer/restic)
 # rm -rf "${HOME}/.cache"
 
 #Check Backup
-/usr/bin/restic -q -r ${BACKUP_LOCATION} check
+/usr/bin/restic -r ${BACKUP_LOCATION} check | lolcat -t 
 
-/usr/bin/restic -q -r ${BACKUP_LOCATION} unlock # Unlock repo
+/usr/bin/restic -q  -r ${BACKUP_LOCATION} unlock # Unlock repo
 
-/usr/bin/restic -q -r ${BACKUP_LOCATION} forget --keep-last 14 --prune # Only keep 14 days
+/usr/bin/restic -q -r ${BACKUP_LOCATION} forget --keep-last 14 --prune | lolcat -t # Only keep 14 days
 
 # Backup the system
-/usr/bin/restic -r ${BACKUP_LOCATION} backup ${HOME} --exclude="${HOME}/.cache" --cleanup-cache=true --no-cache=true --exclude-file=${HOME}/bin/resticExcludes.txt
+/usr/bin/restic -r ${BACKUP_LOCATION} backup ${HOME} --exclude="${HOME}/.cache" --cleanup-cache=true --exclude-file=${HOME}/bin/resticExcludes.txt | lolcat -t
 
 
 #/usr/bin/restic --password-file ${HOME}/.backup_file -r ${BACKUP_LOCATION} check
