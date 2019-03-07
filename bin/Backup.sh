0dcbca8d8aee6ca58a363d21e5a33b39063db5aa
Joseph Harry <findarato@gmail.com>
2019-03-07 12:05:39 -0600

    :bug: fixed backup to run properly Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 014169c08d4f829a281f81fa7d3075cc22e14a21

--- bin/Backup.sh
+++ bin/Backup.sh
@@ -5,20 +5,57 @@ ${HOME}/bin/cleanCache.sh
 BACKUP_LOCATION=/mnt/home/backup/desktop
 
 
-export RESTIC_PASSWORD=$(pass computer/restic)
+export RESTIC_PASSWORD=$(/usr/bin/pass computer/restic)
 
 # rm -rf "${HOME}/.cache"
 
+echo "$(tput sgr0)"
+/usr/bin/restic -r ${BACKUP_LOCATION} unlock  # Unlock repo
+
+echo "$(tput setaf 2)"
+echo "██████╗  █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ ██╗   ██╗██████╗ ";
+echo "██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝ ██║   ██║██╔══██╗";
+echo "██████╔╝███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗██║   ██║██████╔╝";
+echo "██╔══██╗██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║██║   ██║██╔═══╝ ";
+echo "██████╔╝██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝╚██████╔╝██║     ";
+echo "╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝     ";
+
+echo "$(tput sgr0)"
+/usr/bin/restic -r ${BACKUP_LOCATION} backup ${HOME} --exclude="${HOME}/.cache" --cleanup-cache=true --exclude-file=${HOME}/bin/resticExcludes.txt 
+
+
+echo "$(tput setaf 2)"
+echo " ██╗██╗  ██╗    ██████╗  █████╗  ██████╗██╗  ██╗██╗   ██╗██████╗ ███████╗";
+echo "███║██║  ██║    ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██║   ██║██╔══██╗██╔════╝";
+echo "╚██║███████║    ██████╔╝███████║██║     █████╔╝ ██║   ██║██████╔╝███████╗";
+echo " ██║╚════██║    ██╔══██╗██╔══██║██║     ██╔═██╗ ██║   ██║██╔═══╝ ╚════██║";
+echo " ██║     ██║    ██████╔╝██║  ██║╚██████╗██║  ██╗╚██████╔╝██║     ███████║";
+echo " ╚═╝     ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚══════╝";
+
+echo "$(tput sgr0)";
+/usr/bin/restic -r ${BACKUP_LOCATION} forget --keep-daily 14 --prune  # Only keep 14 days
+
+echo "$(tput setaf 2)"
+echo "██████╗ ██████╗ ██╗   ██╗███╗   ██╗███████╗";
+echo "██╔══██╗██╔══██╗██║   ██║████╗  ██║██╔════╝";
+echo "██████╔╝██████╔╝██║   ██║██╔██╗ ██║█████╗  ";
+echo "██╔═══╝ ██╔══██╗██║   ██║██║╚██╗██║██╔══╝  ";
+echo "██║     ██║  ██║╚██████╔╝██║ ╚████║███████╗";
+echo "╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝";
+
+echo "$(tput sgr0)"
+/usr/bin/restic -r ${BACKUP_LOCATION} prune # Cleanup
+
 #Check Backup
-/usr/bin/restic -r ${BACKUP_LOCATION} check | lolcat -t 
 
-/usr/bin/restic -q  -r ${BACKUP_LOCATION} unlock # Unlock repo
+echo "$(tput setaf 2)"
+echo " ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ ";
+echo "██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝ ";
+echo "██║     ███████║█████╗  ██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗";
+echo "██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║";
+echo "╚██████╗██║  ██║███████╗╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝";
+echo " ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ";
 
-/usr/bin/restic -q -r ${BACKUP_LOCATION} forget --keep-last 14 --prune | lolcat -t # Only keep 14 days
 
-# Backup the system
-/usr/bin/restic -r ${BACKUP_LOCATION} backup ${HOME} --exclude="${HOME}/.cache" --cleanup-cache=true --exclude-file=${HOME}/bin/resticExcludes.txt | lolcat -t
-
-
-#/usr/bin/restic --password-file ${HOME}/.backup_file -r ${BACKUP_LOCATION} check
-#/usr/bin/restic --password-file ${HOME}/.backup_file -r ${BACKUP_LOCATION} forget --keep-last 14 --dry-run
+echo "$(tput sgr0)"
+/usr/bin/restic -r ${BACKUP_LOCATION} check
--- bin/backupStatus.sh
+++ bin/backupStatus.sh
@@ -5,5 +5,15 @@ BACKUP_LOCATION=/mnt/home/backup/desktop
 export RESTIC_PASSWORD=$(pass computer/restic)
 
 #Check Backup
+echo "$(tput setaf 2)"
+echo " ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ ";
+echo "██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝ ";
+echo "██║     ███████║█████╗  ██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗";
+echo "██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║";
+echo "╚██████╗██║  ██║███████╗╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝";
+echo " ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ";
+echo "                                                              ";
+
+echo "$(tput sgr0)"
 /usr/bin/restic -q -r ${BACKUP_LOCATION} snapshots
 
