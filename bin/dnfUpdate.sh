5438adc0e6c5e7c89b8db453b0670d72cc51c8bb
Joseph Harry <findarato@gmail.com>
2019-04-09 08:04:17 -0500

    Added unban for Fail to ban - Updated some scripts Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 0767ab1da5cafa4d6788e1c7df0b0bb231956228

--- bin/dnfUpdate.sh
+++ bin/dnfUpdate.sh
@@ -6,10 +6,15 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
+
+
+#Update and clean up dnf
 dnf clean all
 dnf upgrade --best --allowerasing -y;
 dnf upgrade --refresh -y;
 dnf distro-sync --best --allowerasing -y;
 dnf clean all;
+
+# Update and Clean up Flatpak
 flatpak update -y;
 flatpak uninstall --unused -y
