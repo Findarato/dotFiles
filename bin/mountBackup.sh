4ffe1edfd445e30859c98fa166f295d1c4f1dd29
Joseph Harry <findarato@gmail.com>
2019-04-29 09:40:59 -0500

    :bug: feat(backups): Restic backups now use one of two config files Signed-off-by: Joseph Harry <findarato@gmail.com>

--- bin/mountBackup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This is from a previous job, but keeping it for reference
-#mount -t cifs -o joe@library.wpl.lib.in.us //wpl-backup/backup /mnt/wplbackup/ 
--- bin/runBackupDocuments.sh
+++ bin/runBackupDocuments.sh
@@ -4,22 +4,26 @@
 
 ${HOME}/bin/cleanCache.sh
 
-EXCLUDE_FILE="${HOME}/bin/config/resticExcludes.txt"
-
-BACKUP_LOCATION=/run/media/joe/Backup/restic/
-
-BACKUP_LOCATION=/mnt/home/backup/desktop
+if [ -f ${1} ];then
+    source ${1}
+else
+    # Defaulting to work setup
+    EXCLUDE_FILE="${HOME}/bin/config/resticExcludes.txt"
+    BACKUP_LOCATION=/run/media/joe/Backup/restic/
+    BACKUP_LOCATION=/mnt/home/backup/desktop
+    export RESTIC_PASSWORD=$(pass computer/restic)
+    RESTIC=/usr/bin/restic
+fi
 
 echo "$(tput sgr0)"
 
 export RESTIC_PASSWORD=$(pass computer/restic)
 
 if [ ! -d ${BACKUP_LOCATION} ];then
-
-    restic init --repo ${BACKUP_LOCATION}
+    ${RESTIC} init --repo ${BACKUP_LOCATION}
 fi
 
-/usr/bin/restic -r ${BACKUP_LOCATION} unlock  # Unlock repo
+${RESTIC} -r ${BACKUP_LOCATION} unlock  # Unlock repo
 
 echo "$(tput setaf 2)"
 echo "██████╗  █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ ██╗   ██╗██████╗ ";
@@ -29,7 +33,7 @@ echo "██╔══██╗██╔══██║██║     ██╔═
 echo "██████╔╝██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝╚██████╔╝██║     ";
 echo "╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝     ";
 echo "$(tput sgr0)"
-#/usr/bin/restic -r ${BACKUP_LOCATION} backup ${HOME} --tag 🕐 --tag hourly_backup --tag cron --exclude-file=${EXCLUDE_FILE}
-/usr/bin/restic -r ${BACKUP_LOCATION} backup ${HOME}/Documents --tag 📂 --tag 15 --exclude-file=${EXCLUDE_FILE}
+#${RESTIC} -r ${BACKUP_LOCATION} backup ${HOME} --tag 🕐 --tag hourly_backup --tag cron --exclude-file=${EXCLUDE_FILE}
+${RESTIC} -r ${BACKUP_LOCATION} backup ${HOME}/Documents --tag 📂 --tag 15 --exclude-file=${EXCLUDE_FILE}
 
 #notify-send "Restic backup ended."
