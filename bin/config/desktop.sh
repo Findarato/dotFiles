4fa779b0f99a710b1dadb4554debd3aad97e7cc2
Joseph Harry <findarato@gmail.com>
2019-04-29 09:47:20 -0500

    Squash me Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 2268305f4c6b6ef4e318c49e5c60ef637df7af54

--- bin/config/desktop.sh
+++ bin/config/desktop.sh
@@ -4,7 +4,7 @@
 USERNAME=joe
 
 EXCLUDE_FILE="${HOME}/bin/config/resticExcludes.txt"
-BACKUP_LOCATION=/mnt/tarvalon/home/backup
+BACKUP_LOCATION=/mnt/tarvalon/home/backup/desktop
 export RESTIC_PASSWORD=$(pass computer/restic)
 
 # Location of restic
