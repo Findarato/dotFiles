d7815b3ec80b6337d4129d2a4b3c73e2137637fe
Joseph Harry <findarato@gmail.com>
2019-04-30 08:37:30 -0500

    Added the ability to set the version for upgrading Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 9f784ffcb7db942a42c0e044228c97b03fceb084

--- bin/upgrade_version.sh
+++ bin/upgrade_version.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+VERSION=${1}
+
 sudo dnf upgrade -y --refresh
 
 sudo dnf install -y dnf-plugin-system-upgrade
 
-sudo dnf system-upgrade download -y --releasever=28 --allowerasing
+sudo dnf system-upgrade download -y --releasever=${VERSION} --allowerasing
 
 #sudo dnf system-upgrade reboot
