9082325ae893c0b99498f7a53373156c10bf8ae5
Joseph Harry <findarato@gmail.com>
2019-05-17 09:26:40 -0500

    Updated dotfiles with Kitty configs Signed-off-by: Joseph Harry <findarato@gmail.com>

--- scripts/bin/funbox.sh
+++ scripts/bin/funbox.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-podman run --name funbox --rm -it wernight/funbox 
+podman rm funbox
+podman run --name funbox --rm -it wernight/funbox
+#podman run --name funbox --rm -it judavi/funbox
