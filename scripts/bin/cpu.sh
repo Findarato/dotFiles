43a44759204da95ee88e77c75272dd88fa1e5b24
Joseph Harry <findarato@gmail.com>
2019-06-04 12:23:43 -0500

    :fire: Removed some files that should not be in the repo Signed-off-by: Joseph Harry <findarato@gmail.com>

--- scripts/bin/cpu.sh
+++ scripts/bin/cpu.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 clear
+#watch -c -t -n 1 "sensors | grep 'Tdie'| cut -c16-19 | figlet -f future"
 
-watch -c -t -n 1 "sensors | grep 'Tdie'| cut -c16-19 | figlet -f future"
+TEMP=$(sensors | grep 'Package id 0' | cut -c16-20 | bc -l)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+#echo "insert a number"
+#read test
+
+  printf ${RED}
+  printf ${TEMP} 
+#  printf ${TEMP} | figlet -f future
+  printf ${NC}
+
+
