4fcd0554d0c41ceb4d2e9d4a5c44e20e95212dfc
Joseph Harry <findarato@gmail.com>
2019-07-31 20:26:26 -0500

    Fixed move Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 6b6e2da8dada00a053037a888d9507ac9413721c

--- scripts/bin/move.sh
+++ scripts/bin/move.sh
@@ -3,10 +3,10 @@
 DEST=/mnt/tarvalon/storage/TV/
 
 find $PDW \( ! -regex '.*/\..*' \) -type f -name "*.mkv" | sort | while read fname; do
-echo "Transfering ${fname}" | figlet -f future
+echo "Transfering ${fname}" 
 
 rsync -aHAXxvu --numeric-ids --progress ${fname} ${DEST}
-echo "Removing ${fname}" | figlet -f future
+echo "Removing ${fname}" 
 rm -f ${fname}
 
 done
