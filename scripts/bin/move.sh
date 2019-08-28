7b7be5ede227ea098a861d3ccf16da824995cd7b
Joseph Harry <findarato@gmail.com>
2019-08-28 17:59:20 -0500

    Updated the move script
    Signed-off-by: Joseph Harry <findarato@gmail.com>

--- scripts/bin/move.sh
+++ scripts/bin/move.sh
@@ -3,11 +3,11 @@
 DEST=/mnt/tarvalon/storage/TV/
 
 find $PDW \( ! -regex '.*/\..*' \) -type f -name "*.mkv" | sort | while read fname; do
-echo "Transfering ${fname}" | figlet -f future
+echo "Transfering ${fname}"
 
 rsync -aHAXxvu --numeric-ids --progress ${fname} ${DEST}
-echo "Removing ${fname}" | figlet -f future
-rm -f ${fname}
+#echo "Removing ${fname}" | figlet -f future
+#rm -f ${fname}
 
 done
 
