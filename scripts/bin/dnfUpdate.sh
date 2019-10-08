5296cc4a96f8d06306a10ea0795e64544191d96f
Joseph Harry <findarato@gmail.com>
2019-10-08 10:06:48 -0500

    :sparkle: Cleaning stuff up Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: 5c312ac34ecb92dcb3cdfb6f561f4dc8d379937f

--- scripts/bin/dnfUpdate.sh
+++ scripts/bin/dnfUpdate.sh
@@ -7,11 +7,11 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 #Update and clean up dnf
-dnf clean all
+#dnf clean all
 dnf upgrade --best --allowerasing -y;
 
 dnf distro-sync --best --allowerasing -y;
-dnf clean all;
+#dnf clean all;
 
 # Update and Clean up Flatpak
 #sudo su -c "flatpak update -y" jharry;
