7069ce97890fae32ddef53bbdee0db2451bf2d1b
Joseph Harry <findarato@gmail.com>
2019-12-06 12:01:56 -0600

    :sparkle: Updated i3 Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: bb69397baa7132801368c48d0125ff4032120d4a

--- scripts/bin/removeTracker.sh
+++ scripts/bin/removeTracker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 tracker daemon -t
+mkdir -p ~/.config/autostart/
 cp /etc/xdg/autostart/tracker* ~/.config/autostart/
 sed -i 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/' ~/.config/autostart/tracker*
---
+++ scripts/bin/restore_latest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Started December 6, 2019
+# Requires restic
+# This should allow restoring with a configuration file
+## example run restore_latest.sh work
+
+${HOME}/bin/cleanCache.sh
+
+if [ -f "${1}" ];then
+    source "${1}"
+else
+    if [ -f "${HOME}/bin/config/${1}.sh" ];then
+        source "${HOME}/bin/config/${1}.sh"
+    else
+        echo "Unable to load config file"
+	      notify-send "Error" "Unable to load config file"
+        exit;
+    fi
+fi
+
+${RESTIC} -r ${BACKUP_LOCATION} unlock  # Unlock repo
+
+#${RESTIC} -r ${BACKUP_LOCATION} restore latst --target ${HOME}   # Unlock repo
+
+
+#${RESTIC} -r ${BACKUP_LOCATION} unlock  # Unlock repo
+
+#${RESTIC} -r ${BACKUP_LOCATION} backup ${HOME} --tag 🕐 --tag hourly --exclude-file=${EXCLUDE_FILE} --verbose 2
