5b953ab6c4aa6de3413e27deeed7be7ed93d15f4
Joe <findarato@gmail.com>
2020-05-08 12:54:31 -0500

    Updated work Signed-off-by: Joe <findarato@gmail.com>
    Former-commit-id: a0a718df2979c1fc92228ce47539a7272747ecaf [formerly 83269f2515543c5f7edfeccf3d6c78e07ce7c8ff]
    Former-commit-id: f95a3cfb407d21903453b31fcb3ee4e5de086e03

--- scripts/bin/config/work.sh
+++ scripts/bin/config/work.sh
@@ -15,5 +15,5 @@ DAYS=30
 WEEKS=1
 MONTHS=1
 
-RUNBEFORE="echo 'mounting Box';rclone mount Box:/backup/laptop /mnt/box --daemon"
+RUNBEFORE="echo 'mounting Box';rclone mount Box:/backup/desktop /mnt/box --daemon"
 RUNAFTER="echo 'Unmounting Box';fusermount -u /mnt/box"
