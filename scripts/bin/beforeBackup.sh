72d0420a1c5558c80a75aa81a5d21506a94d4ae3
Joe <findarato@gmail.com>
2020-05-08 11:00:18 -0500

    Converting beforeBackup to take a script Signed-off-by: Joe <findarato@gmail.com>

--- scripts/bin/beforeBackup.sh
+++ scripts/bin/beforeBackup.sh
@@ -11,19 +11,35 @@ pkill --signal 9 Enpass
 NOW=$(date +%u)
 #updateRepos.sh
 
-
 "${HOME}/bin/cleanCache.sh"
 #"${HOME}/bin/docker_clean.sh"
+
+if [ -f "${1}" ];then
+    source "${1}"
+else
+    if [ -f "${HOME}/bin/config/${1}.sh" ];then
+        source "${HOME}/bin/config/${1}.sh"
+        "${HOME}/bin/Backup.sh" ${1}
+    else
+        echo "Unable to load config file"
+	    notify-send "Error" "Unable to load config file"
+        exit;
+    fi
+fi
 
 # Clean up Evolution
 rm -rf "${HOME}/.cache/evolution/mail/"
 
 # Backup Evolution
-/usr/libexec/evolution/evolution-backup --display=:0 --restart --backup /mnt/home/backup/evolution/evolution_backup_${NOW}.tar.gz
-/usr/libexec/evolution/evolution-backup --display=:0 --restart --backup /mnt/box/evolution/evolution_backup_${NOW}.tar.gz
+# Need to think about this one
+# /usr/libexec/evolution/evolution-backup --display=:0 --restart --backup "${HOME}/evolution/evolution_backup_${NOW}.tar.gz"
+# /usr/libexec/evolution/evolution-backup --display=:0 --restart --backup /mnt/box/evolution/evolution_backup_${NOW}.tar.gz
 
 #Call Backup script
-"${HOME}/bin/Backup.sh" work
+
+
+
+
 
 # Attemping to restore some of them
 
