19bdfb84bf0d8fd96f09f099387b310281679eeb
Joe <findarato@gmail.com>
2020-05-26 10:39:10 -0500

    :sparkle: Added a config for my desktop Signed-off-by: Joe <findarato@gmail.com>

--- scripts/bin/docker_clean.sh
+++ scripts/bin/docker_clean.sh
@@ -2,16 +2,15 @@
 
 
 
-if hash docker 2>/dev/null; then
-  # Delete all containers
-  docker rm $(docker ps -a -q)
-  # Delete all images
-  docker rmi $(docker images -q)
-fi
-
 if hash podman 2>/dev/null; then
   # Delete all containers
   podman rm $(podman ps -a -q)
   # Delete all images
   podman rmi $(podman images -q)
+
+elif hash docker 2>/dev/null; then
+  # Delete all containers
+  docker rm $(docker ps -a -q)
+  # Delete all images
+  docker rmi $(docker images -q)
 fi
