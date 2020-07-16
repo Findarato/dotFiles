cdb064fda837a268e2a567ce0dd4aa583f4fdfe3
Joe <findarato@gmail.com>
2020-07-16 07:00:54 -0500

    :sparkle: So much updating, mainly around support for pywal Signed-off-by: Joe <findarato@gmail.com>

--- scripts/bin/wal_watch.sh
+++ scripts/bin/wal_watch.sh
@@ -6,7 +6,7 @@ inotifywait -m -e create -e moved_to ${1}|
         # echo "The file '$file' appeared in directory '$dir' via '$action'"
         echo "~/.local/bin/wal --backend colorz -n -i ${dir}${file}"
         "${HOME}/.local/bin/wal" -c
-        "${HOME}/.local/bin/wal" --backend colorz -n -i "${dir}${file}"
+        "${HOME}/.local/bin/wal" -q --backend colorz -n -i "${dir}${file}"
         # "${HOME}/.local/bin/wal" --backend colorz -n -i "${dir}${file}"
         # do something with the file
     done
