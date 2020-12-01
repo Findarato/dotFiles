961961b4fc0b9760906c502405fbc4d487959e10
Joseph Harry <findarato@gmail.com>
2020-12-01 07:37:25 -0600

    :sparkle: Added some scripts and adjusted htop Signed-off-by: Joseph Harry <findarato@gmail.com>

---
+++ scripts/bin/cpu_speed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+
+#watch -n 1 'cat /proc/cpuinfo |grep MHz'
+
+SPEED=$(cat /proc/cpuinfo | grep 'MHz' | cut -c12-20 | bc -l)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+#echo "insert a number"
+#read test
+
+printf ${RED}
+printf ${SPEED} 
+#  printf ${TEMP} | figlet -f future
+printf ${NC}
