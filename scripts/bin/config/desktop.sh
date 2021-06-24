35a00489e58edea31814f65057e0361dc548ab86
Joseph Harry <findarato@gmail.com>
2021-06-24 08:04:03 -0500

    Updating from desktop Signed-off-by: Joseph Harry <findarato@gmail.com>

--- scripts/bin/config/desktop.sh
+++ scripts/bin/config/desktop.sh
@@ -6,8 +6,12 @@ USERNAME=joe
 EXCLUDE_FILE="${HOME}/bin/config/resticExcludes.txt"
 #BACKUP_LOCATION=/mnt/tarvalon/home/backup/desktop
 #BACKUP_LOCATION=/mnt/tarvalon/storage/Backup/desktop
-BACKUP_LOCATION=/mnt/blacktower/backup/joe/desktop
+#BACKUP_LOCATION=/mnt/blacktower/backup/joe/desktop
+BACKUP_LOCATION=s3:https://blacktower.telaranrhiod.space:9000/desktop-backup
+
 export RESTIC_PASSWORD=$(pass computer/restic)
+export AWS_ACCESS_KEY_ID=$(pass computer/blacktower/s3Access)
+export AWS_SECRET_ACCESS_KEY=$(pass computer/blacktower/s3AccessSecret)
 
 # Location of restic
 RESTIC=/usr/bin/restic
