c864ab147347b71249cb2c4443a872a2f7a5195b
Joseph Harry <findarato@gmail.com>
2022-02-14 07:55:46 -0600

    :sparkle: Adding S3 form work Signed-off-by: Joseph Harry <findarato@gmail.com>
    Former-commit-id: f97ee29e77b97fbf8cbe0e86d31307a6e3cc142c
    Former-commit-id: 8c814c4e174a1384c7080d94ece80a4588b700e2
    Former-commit-id: edea7757d69265bfa3dc7f2a36c9c47ac58ff574

--- scripts/bin/config/work.sh
+++ scripts/bin/config/work.sh
@@ -4,9 +4,14 @@
 USERNAME=jharry
 EXCLUDE_FILE="${HOME}/bin/config/resticExcludes.txt"
 # BACKUP_LOCATION=/mnt/home/backup/desktop
-BACKUP_LOCATION=/mnt/box/
-export RESTIC_PASSWORD=$(pass computer/restic)
+#BACKUP_LOCATION=/mnt/box/
+BACKUP_LOCATION=s3:https://itc-tn-01.methnet.org:9000/joe.backup
+BACKUP_SRC=/home/jharry/
 
+
+export RESTIC_PASSWORD=$(pass computer/restic)
+export AWS_ACCESS_KEY_ID=$(pass computer/itc-tn-01/s3Access)
+export AWS_SECRET_ACCESS_KEY=$(pass computer/itc-tn-01/s3AccessSecret)
 # Location of restic
 RESTIC=/usr/bin/restic
 
@@ -16,5 +21,9 @@ DAYS=30
 WEEKS=1
 MONTHS=12
 
-RUNBEFORE="echo 'mounting Box';rclone mount Box:/backup/desktop /mnt/box --daemon; sleep 20"
-RUNAFTER="echo 'Unmounting Box';sleep 60;fusermount -u /mnt/box"
+
+#RUNBEFORE="echo 'mounting Box';rclone mount Box:/backup/desktop /mnt/box --daemon; sleep 20"
+#RUNAFTER="echo 'Unmounting Box';sleep 60;fusermount -u /mnt/box"
+
+RUNBEFORE=""
+RUNAFTER=""
