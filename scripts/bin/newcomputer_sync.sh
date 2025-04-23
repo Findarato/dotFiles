fa9ef44c608e9af3c331c93ddb78821af8faa1c6
Joseph Harry <findarato@gmail.com>
2025-04-23 08:43:44 -0500

    :sparkles: Added a container script to restore the new computer Signed-off-by: Joseph Harry <findarato@gmail.com>

---
+++ scripts/bin/newcomputer_sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Started December 26, 2024
+# Requires restic
+# This should allow restoring with a configuration file
+## example run restore_all.sh work
+
+
+/usr/bin/distrobox-enter --name fedora -e '/var/home/joe/bin/restore_all.sh desktop'
