03b9c8e129f53a80d2bb0539566ccb179e03c0b5
Joseph Harry <findarato@gmail.com>
2025-06-27 13:49:53 -0500

    Updated a few things Signed-off-by: Joseph Harry <findarato@gmail.com>

---
+++ scripts/bin/rhel_devel_renew.sh
@@ -0,0 +1,6 @@
+sudo subscription-manager remove --all
+sudo subscription-manager unregister
+sudo subscription-manager clean
+sudo subscription-manager register
+sudo subscription-manager refresh
+sudo subscription-manager attach --auto
--- scripts/bin/speedTest.sh
+++ scripts/bin/speedTest.sh
@@ -1,4 +1,4 @@
 #iperf3 -tinf -c 10.1.1.5
 #iperf3 -tinf -c 10.1.10.1
 #iperf3 -tinf -c 10.1.1.1
-iperf3 -P 4 --bidir -tinf -c 10.1.1.5
+iperf3  --bidir -tinf -c 10.1.1.1
