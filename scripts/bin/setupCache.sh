e7f95a2e74ea397d85f5034c32635d4e856092bb
Joseph Harry <findarato@gmail.com>
2025-07-25 15:22:34 -0500

    :sparkles: feat(cache): Flatpak Firefox and Zen are now RAM cached Signed-off-by: Joseph Harry <findarato@gmail.com>

--- scripts/bin/setupCache.sh
+++ scripts/bin/setupCache.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env bash
-DIRPATH="/mnt/cache"
-APPS="${DIRPATH}/${USER}/apps"
-BROWSERS="${DIRPATH}/${USER}/browsers"
-FLATPAK="${DIRPATH}/${USER}/flatpak"
+CACHEDIR="/mnt/cache"
+APPS="${CACHEDIR}/${USER}/apps"
+BROWSERS="${CACHEDIR}/${USER}/browsers"
+FLATPAK="${CACHEDIR}/${USER}/flatpak"
 
-if [ -d "${DIRPATH}" ]; then
+if [ -d "${CACHEDIR}" ]; then
     if [ ! -d "${APPS}" ]; then
-        rm -rf ${HOME}/.cache/{google-chrome,libgweather,mozilla,shotwell,thumbnails,vivaldi,google-chrome-beta,google-chrome-unstable,chromium}
-        rm -rf ${HOME}/.cache/{wal,shotwell,thumbnails,libgweather,thunderbird}
-        mkdir -p $BROWSERS/{google-chrome,mozilla,vivaldi,google-chrome-beta,google-chrome-unstable,chromium}
-        mkdir -p $APPS/{shotwell,thumbnails,libgweather,thunderbird,mpv/watch_later}
-        ln -s $BROWSERS/{google-chrome,mozilla,vivaldi,google-chrome-beta,google-chrome-unstable,chromium} ${HOME}/.cache/
-        ln -s $APPS/{shotwell,thumbnails,libgweather,thunderbird} ${HOME}/.cache/
+      rm -rf ${HOME}/.cache/{google-chrome,libgweather,mozilla,shotwell,thumbnails,vivaldi,google-chrome-beta,google-chrome-unstable,chromium}
+      rm -rf ${HOME}/.cache/{wal,shotwell,thumbnails,libgweather,thunderbird}
+      mkdir -p $BROWSERS/{google-chrome,mozilla,vivaldi,google-chrome-beta,google-chrome-unstable,chromium}
+      mkdir -p $APPS/{shotwell,thumbnails,libgweather,thunderbird,mpv/watch_later}
+      ln -s $BROWSERS/{google-chrome,mozilla,vivaldi,google-chrome-beta,google-chrome-unstable,chromium} ${HOME}/.cache/
+      ln -s $APPS/{shotwell,thumbnails,libgweather,thunderbird} ${HOME}/.cache/
 
       # Flatpak Versions
-      # Firefox
 
+      # Firefox
+      flatpak override org.mozilla.firefox --user --filesystem=${FLATPAK}/org.mozilla.firefox/cache
       rm -rf ${HOME}/.var/app/org.mozilla.firefox/cache
       mkdir -p ${FLATPAK}/org.mozilla.firefox/cache
       ln -s ${FLATPAK}/org.mozilla.firefox/cache ${HOME}/.var/app/org.mozilla.firefox/cache
+
+      # app.zen_browser.zen
+      flatpak override app.zen_browser.zen --user --filesystem=${FLATPAK}/app.zen_browser.zen/cache
+      rm -rf ${HOME}/.var/app/app.zen_browser.zen/cache
+      echo "${FLATPAK}/app.zen_browser.zen/cache"
+      mkdir -p ${FLATPAK}/app.zen_browser.zen/cache
+      ln -s ${FLATPAK}/app.zen_browser.zen/cache ${HOME}/.var/app/app.zen_browser.zen/cache
+
     fi
 fi
+
+      flatpak override org.mozilla.firefox --user --filesystem=${FLATPAK}/org.mozilla.firefox/cache
+      rm -rf ${HOME}/.var/app/org.mozilla.firefox/cache
+      mkdir -p ${FLATPAK}/org.mozilla.firefox/cache
+      ln -s ${FLATPAK}/org.mozilla.firefox/cache ${HOME}/.var/app/org.mozilla.firefox/cache
