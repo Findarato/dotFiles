46db8e5d673db6478a580902a58ef96b91d42d0a
Joseph Harry <findarato@gmail.com>
2025-09-11 10:11:28 -0500

    :fire: Removed some folders that are no longer valid - :fire: rofi is for I3 and wayland is the way - :fire: No longer using Tilix and no one should - :chore: Added some of the new functionality info topgrade config Signed-off-by: Joseph Harry <findarato@gmail.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -21,15 +21,44 @@ config.line_height = 1.2
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 40
+config.initial_cols = 80
+config.initial_rows = 12
 
 -- config.color_scheme = 'AdventureTime'
 config.color_scheme = 'Dracula (Official)'
 config.color_scheme = 'Dracula+'
 
 config.colors = {
-  cursor_bg = "purple",
+ cursor_bg = "purple",
+ cursor_border = "purple",
+}
+
+-- No titlebar or windows
+config.window_decorations = 'RESIZE'
+config.enable_tab_bar = true
+
+-- key bindings
+
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CloseCurrentPane { confirm = false},
+  },
+  {
+    key = '%',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain'},
+  },
+  {
+    key = '"',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain'},
+  },
+  
+
+
+
 }
 
 -- Finally, return the configuration to wezterm:
