ded8d96ae32824df3485e9ac71d60c317b514033
Joseph Harry <findarato@gmail.com>
2026-03-10 11:25:45 -0500

    Few edits Signed-off-by: Joseph Harry <findarato@gmail.com>

--- scripts/bin/olama.sh
+++ scripts/bin/olama.sh
@@ -1,5 +1,7 @@
-docker stop ollama
-docker rm ollama
+#docker stop ollama
+#docker rm ollama
 docker run -d --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:rocm
 
 docker exec -it ollama ollama run llama3
+
+docker stop ollama && docker rm ollama
