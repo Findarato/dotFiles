52e9be02e053ff1bb392ccc9206e0c94816eb62f
Joseph Harry <findarato@gmail.com>
2026-03-19 10:29:33 -0500

    :sparkle: Updated ollama script to work with different modles Signed-off-by: Joseph Harry <findarato@gmail.com>

--- scripts/bin/olama.sh
+++ scripts/bin/olama.sh
@@ -1,7 +1,29 @@
-#docker stop ollama
-#docker rm ollama
-docker run -d --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:rocm
+#!/usr/bin/env bash
+#
+#        ,--.
+#       /_ | |
+#        ||| |
+#  __   | || |
+# /_ |__||_|| |
+#    |___|__|
+#
+#  ollama run llama2-70b-chat:70b
 
-docker exec -it ollama ollama run llama3
+docker stop ollama
+docker rm ollama
 
-docker stop ollama && docker rm ollama
+docker run -d --device /dev/kfd \
+--device /dev/dri \
+-v ollama:/root/.ollama \
+-p 11434:11434 --name ollama ollama/ollama:rocm
+
+#docker exec -it ollama ollama run llama3
+
+
+docker exec -it ollama ollama pull llama3.1:8b
+
+docker exec -it ollama ollama pull qwen2.5-coder:1.5b-base
+
+docker exec -it ollama ollama pull nomic-embed-text:latest
+
+docker logs -f ollama && docker stop ollama && docker rm ollama
